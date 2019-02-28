b7a9a5f94cc060cc663bddc022301f5da5087d93
DESKTOP-Infobase\Pedro <pedro.borges@infobase.com.br>
2019-02-28 12:06:03 -0500

    Challenge TWO

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -1,4 +1,6 @@
 // Pedro Henrique Borges Lopes (C0740134) and Flavio Noe dos Santos (C0740832)
+//Challenge One
+
 function ChallengeOne(initial, until, count) {
     let result =0;
     let list = [];
@@ -15,3 +17,14 @@ function ChallengeOne(initial, until, count) {
 const result = ChallengeOne(1109, 119, 11);
 
 console.log("Challenge One:", result);
+
+//Challenge Two
+
+function ChallengeTwo() {
+
+    var initial = prompt("Please enter the start Number");
+    var final = prompt("Please enter the final Number");
+    var step = prompt("Please enter the step down Number");
+
+    console.log("Challenge TWO:", ChallengeOne(parseInt(initial), parseInt(final), parseInt(step)));
+}
